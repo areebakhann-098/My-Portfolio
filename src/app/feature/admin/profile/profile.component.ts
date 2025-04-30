925562623b707ca83bdd21f7e506c856abad3634
areeba-wq <areebakhalid9854@gmail.com>
2025-04-30 16:51:04 +0500

    update

--- src/app/feature/admin/profile/profile.component.ts
+++ src/app/feature/admin/profile/profile.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './profile.component.html'
 })
 export class ProfileComponent {
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    address: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    facebook: new FormControl(''),
+    instagram: new FormControl(''),
+    github: new FormControl(''),
+    linkedin: new FormControl(''),
+    fiverr: new FormControl('')
+  });
 
+  submitForm() {
+    if (this.profileForm.valid) {
+      console.log('Profile Data:', this.profileForm.value);
+      this.profileForm.reset();
+    }
+  }
 }
